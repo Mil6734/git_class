086ed3716c3d69dca6eed10d126d7d6d8325c4b6
andmil <mil73197@gmail.com>
2023-02-08 14:50:05 +0300

    git_class3

--- class8/main8.py
+++ class8/main8.py
@@ -1,1 +1,31 @@
-print("hello")
+# import math
+#
+# num = math.sqrt(4)
+# num2 = math.ceil(3.2)
+# num3 = math.pi
+# print(num)
+# print(num2)
+# print(num3)
+
+# import math
+# a = int(input("Радиус окружности"))
+# s = round(2 * math.pi * a, 2)
+# print("Длинна окружности", s)
+
+
+# import time
+# import locale
+# locale.setlocale(locale.LC_ALL, "ru")
+#
+# s = time.time()
+# print(s)
+#
+# local = time.ctime(3011200067)
+# print(local)
+#
+# res = time.localtime()
+# print(res)
+#
+# print(time.strftime("%B %d (%A), %Y."))
+
+
